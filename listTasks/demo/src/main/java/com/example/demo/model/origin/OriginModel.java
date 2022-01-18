0330f810e589fbf67f483328ded483bcbe9a4874
eugenesation <eugenesmirnov03@gmail.com>
2022-01-18 18:46:01 +0200

    entity has been added

--- listTasks/demo/src/main/java/com/example/demo/model/origin/OriginModel.java
+++ listTasks/demo/src/main/java/com/example/demo/model/origin/OriginModel.java
@@ -2,10 +2,16 @@ package com.example.demo.model.origin;
 
 import com.example.demo.model.character.CharacterModel;
 
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "origins")
 public class OriginModel {
     private String name;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @OneToOne
     private CharacterModel characterModel;
@@ -13,6 +19,14 @@ public class OriginModel {
     private String url;
 
     public OriginModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
